792e6473309fe53a247ea1cfdbacfdd888a8b99b
James <james86.dev@gmail.com>
2025-07-26 12:14:37 +0200

    init

--- src/main/java/io/github/james86dev/easyVoidGen/VoidGen.java
+++ src/main/java/io/github/james86dev/easyVoidGen/VoidGen.java
@@ -1,17 +1,7 @@
 package io.github.james86dev.easyVoidGen;
 
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.generator.WorldInfo;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Random;
 
 public class VoidGen extends ChunkGenerator {
-
-    @Override
-    public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkData chunkData) {
-        //:)
-    }
-
 
 }
